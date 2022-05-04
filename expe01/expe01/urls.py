9058b90d6e99ac09af82fe797866b05889ebc2af
poppop001 <ullaulla01@naver.com>
2022-05-04 10:45:35 +0900

    instamark

--- expe01/expe01/urls.py
+++ expe01/expe01/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import Sub
 
+
 urlpatterns = [
     path('', Sub.as_view()),
 ]
--- expe01/expe01/views.py
+++ expe01/expe01/views.py
@@ -3,4 +3,8 @@ from rest_framework.views import APIView
 
 class Sub(APIView):
     def get(self, request):
+        print('겟으로 호출')
+        return render(request, 'expe01/main.html')
+    def post(self, request):
+        print('포스트로 호출')
         return render(request, 'expe01/main.html')
